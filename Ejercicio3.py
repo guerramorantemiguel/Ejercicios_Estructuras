ba40ca2d4d076d20c3404a593985065e13247fa9
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-17 10:47:40 +0000

    código

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,18 +3,21 @@ class Producto:
     self.precio_bruto_producto = 100
   def facturar(self):
     precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA
-    
-
 IVA_alimentacion = 0,055
-IVA_servicios = 0,20
+IVA_servicios = 0,20   
+class Producto_Servicio:
+  def __init__(self, precio_neto_producto):
+    precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_servicios
+    print(precio_neto_producto)
+class Producto_Alimenticio:
+    def __init__(self, precio_neto_producto):
+    precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_alimentacion
+    print(precio_neto_producto)
+  
 
 
-if  producto(naturaleza.servicios):
-  precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_servicios
-  print(precio_neto_producto)
-if producto(naturaleza.alimentacion):
-  precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_alimentacion
-  print(precio_neto_producto)
+
+
   
 
 
