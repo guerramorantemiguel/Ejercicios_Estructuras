808b40459f5c47c755f7df605e299d751433d086
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-17 09:19:19 +0000

    código

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,6 +1,16 @@
-producto = 
+
 precio_bruto_producto = 100
 IVA_alimentacion = 0,055
 IVA_servicios = 0,20
 
 precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA
+
+if  producto(naturaleza.servicios):
+  precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_servicios
+  print(precio_neto_producto)
+if producto(naturaleza.alimentacion):
+  precio_neto_producto = precio_bruto_producto + precio_bruto_producto * IVA_alimentacion
+  print(precio_neto_producto)
+  
+
+
