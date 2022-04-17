0a3addaa0205d6d88e7ad10c408545dde3aafb33
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-17 13:47:38 +0000

    código

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,3 +2,8 @@ class Texto:
   def __init__(self, linea_1, linea_2):
     self.linea_1 = linea_1
     self.linea_2 = linea_2
+  def mayusculas(self):
+    self.linea_1 = self.linea_1.upper()
+    self.linea_2 = self.linea_2.upper()
+    
+    
